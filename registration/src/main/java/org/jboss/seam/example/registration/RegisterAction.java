403d2a39b0ae144bb028a8986869184542aca88a
Marek Novotný <hotmana76@gmail.com>
2014-07-30 14:41:50 +0200

    added Registration example

---
+++ registration/src/main/java/org/jboss/seam/example/registration/RegisterAction.java
@@ -0,0 +1,57 @@
+package org.jboss.seam.example.registration;
+
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Named("register")
+@Stateless
+public class RegisterAction {
+
+	private final User newUser = new User();
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	private Logger log = Logger.getLogger(RegisterAction.class.getCanonicalName());
+
+	private UIInput usernameInput;
+
+	public UIInput getUsernameInput() {
+		return usernameInput;
+	}
+
+	public void setUsernameInput(UIInput usernameInput) {
+		this.usernameInput = usernameInput;
+	}
+
+	@Produces
+	@Named
+	public User getNewUser() {
+		return newUser;
+	}
+
+	public String register() {
+		User existingUser = em.find(User.class, newUser.getUsername());
+
+		if (existingUser == null) {
+			em.persist(newUser);
+			log.info("Registered new user #{newUser.username}");
+			return "/registered.xhtml";
+		} else {
+			String message = "User " + newUser.getUsername() + " already exists";
+			FacesMessage facesMessage = new FacesMessage(message);
+			FacesContext.getCurrentInstance().addMessage(null,  facesMessage);
+			log.warning(message);
+			return null;
+		}
+	}
+
+}
---
+++ registration/src/main/java/org/jboss/seam/example/registration/User.java
@@ -0,0 +1,70 @@
+package org.jboss.seam.example.registration;
+
+import java.io.Serializable;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Named;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+@Named("user")
+@Model
+@Table(name="users")
+public class User implements Serializable {
+
+	
+	private String username;
+	private String password;
+	private String name;
+	private static final long serialVersionUID = 1L;
+
+	public User() {
+	//	super();
+	}
+	
+	public User(String name, String password, String username) {
+		this.name = name;
+		this.password = password;
+		this.username = username;
+	}
+	
+	@Id @NotNull @Size(min=5, max=15)
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}   
+	
+	@NotNull @Size(min=5, max=15)
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}   
+	
+	@NotNull
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+   
+	@Override
+	public String toString() {
+		return "User(" + username + ")";
+	}
+}
